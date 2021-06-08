ed3b56829c38b9f713a5eb24ae8237750a4df225
CA-Chambers <chambers.ceddrick@live.jobcorps.org>
2021-06-08 20:10:29 +0000

    hay

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -29,7 +29,7 @@ function runProgram(){
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);  // change 'eventType' to the type of event you want to handle
-
+  
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -55,10 +55,21 @@ function runProgram(){
       } else if (event.which === KEY.RIGHT){
            speedX = 5;
       } else{
-          
+
           speedY = 5;
   }
   }
+  function handleKeyup(event) {
+      if (event.which === KEY.LEFT){
+          speedX = 0;
+      } else if (event.which === KEY.UP){
+           speedY = 0;
+      } else if (event.which === KEY.RIGHT){
+           speedX = 0;
+      } else{
+          speedY = 0;
+      }
+    }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
